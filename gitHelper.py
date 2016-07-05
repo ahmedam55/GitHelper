3a08ae89056b36b7e2c8d8d95a0ed80bff6838fa
ahmedam66 <ahmed42d44@gmail.com>
2016-07-05 15:20:33 +0400

    dash missed

--- gitHelper.py
+++ gitHelper.py
@@ -11,42 +11,42 @@ class logCommand(sublime_plugin.WindowCommand):
 
 class ddCommand(sublime_plugin.WindowCommand):
     def run(self):
-        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git -no-color difftool -d'
+        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git --no-color difftool -d'
         sublime.set_clipboard(cmd_line)
         self.window.run_command('open_terminal')
 
 
 class dfCommand(sublime_plugin.WindowCommand):
     def run(self):
-        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git -no-color difftool '+self.window.active_view().file_name()+' -y'
+        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git --no-color difftool '+self.window.active_view().file_name()+' -y'
         sublime.set_clipboard(cmd_line)
         self.window.run_command('open_terminal')
 
  
 class bfCommand(sublime_plugin.WindowCommand):
     def run(self):
-        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git -no-color blame '+self.window.active_view().file_name()+'>h&&subl -w h && rm h'
+        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git --no-color blame '+self.window.active_view().file_name()+'>h&&subl -w h && rm h'
         sublime.set_clipboard(cmd_line)
         self.window.run_command('open_terminal')
 
 
 class cfCommand(sublime_plugin.WindowCommand):
     def run(self):
-        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git -no-color checkout '+self.window.active_view().file_name()
+        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git --no-color checkout '+self.window.active_view().file_name()
         sublime.set_clipboard(cmd_line)
         self.window.run_command('open_terminal')
 
 
 class acCommand(sublime_plugin.WindowCommand):
     def run(self):
-        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git -no-color update-index --assume-unchanged '+self.window.active_view().file_name()
+        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git --no-color update-index --assume-unchanged '+self.window.active_view().file_name()
         sublime.set_clipboard(cmd_line)
         self.window.run_command('open_terminal')
 
 
 class auCommand(sublime_plugin.WindowCommand):
     def run(self):
-        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git -no-color update-index --no-assume-unchanged '+self.window.active_view().file_name()
+        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&& git --no-color update-index --no-assume-unchanged '+self.window.active_view().file_name()
         sublime.set_clipboard(cmd_line)
         self.window.run_command('open_terminal')
 
@@ -56,7 +56,7 @@ class shCommand(sublime_plugin.WindowCommand):
         beginline=str(self.window.active_view().rowcol(self.window.active_view().sel()[0].begin())[0]+1)
         endline=str(self.window.active_view().rowcol(self.window.active_view().sel()[0].end())[0]+1)
 
-        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&&  git -no-color log --pretty=short -u -L '+beginline+','+endline+':'+self.window.active_view().file_name()+'>h && subl -w h && rm h'
+        cmd_line = 'cd ' +self.window.project_data()['folders'][0]['path'] +'&&  git --no-color log --pretty=short -u -L '+beginline+','+endline+':'+self.window.active_view().file_name()+'>h && subl -w h && rm h'
         sublime.set_clipboard(cmd_line)
         self.window.run_command('open_terminal')
 
